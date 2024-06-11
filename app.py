ca12ee31901e63a3c16a8314ef3438caa127cf73
cakusers <cakra.kusuma.erlangga-2021@fst.unair.ac.id>
2024-06-11 19:11:30 +0700

    Image are uploaded to cloud bucket and can predict image from that

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import os
 from uuid import uuid4
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import sys
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import numpy as np
+from google.cloud import storage
+from google.oauth2 import service_account
 
+from PIL import Image
+import numpy as np
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Config
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+CREDENTIALS = service_account.Credentials.from_service_account_file(os.environ.get('CREDENTIAL_PATH'))
+CLIENT = storage.Client(credentials=CREDENTIALS)
+BUCKET = CLIENT.bucket(os.environ.get('BUCKET_NAME'))
 
 model = load_model('./model/model.h5')
 
@@ -33,30 +39,45 @@ def predict(model, image):
 @app.route('/predict', methods=['POST'])
 def post_predict():
     # print(request, file=sys.stdout)
-    if 'file' not in request.files :
+    if 'image' not in request.files :
         return jsonify({"message": "Please insert image"}), 400
     
-    file = request.files['file']
+    file = request.files['image']
     
     if file.filename == '' :
         return jsonify({"message": "Please insert image"}), 400
     
+    # Upload to Bucket
     if file and allowed_file(file.filename):
         rand_name = uuid4().hex
         file_extension = os.path.splitext(file.filename)[1]
         filename = secure_filename(''.join([rand_name, file_extension]))
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        destination_path = ''.join(['uploads/', filename])
+        blob = BUCKET.blob(destination_path)
+        generation_match_precondition = 0
+        
+        try :
+            blob.upload_from_file(file, if_generation_match=generation_match_precondition)
+            
+        except :
+            return jsonify({"message" : "Filename already exist"}), 400
+        
+    else :
+        return jsonify({"message": "Unsuported file format"}), 400
     
     # Image
-    img_path = ''.join(['uploads/', filename])
-    image = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
+    image = Image.open(file)
+    image = image.resize((224, 224))
     image = tf.keras.preprocessing.image.img_to_array(image) / 255.0
     
-    prediction = predict(model, image)
-    print(f'Predicted class: {prediction}', file=sys.stdout)
-
-    return jsonify({"message" : "Success", "prediction" : str(prediction)}), 200
+    try :
+        prediction = predict(model, image)
+        return jsonify({"message" : "Success", "prediction" : str(prediction)}), 200
+    
+    except:
+        return jsonify({"message" : "Server error"}), 500
+    
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host=os.environ.get('HOST'), port=os.environ.get('PORT'), debug=True)
  
